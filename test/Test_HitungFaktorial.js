c8a44006a35597ef961c476d72280654818e623e
Gutserk <ardanasmirza@gmail.com>
2024-10-18 22:37:44 +0700

    update question

--- test/Test_HitungFaktorial.js
+++ test/Test_HitungFaktorial.js
@@ -1,18 +1,24 @@
 const assert = require('assert');
 const faktorial = require('../question/HitungFaktorial');
 
-
 describe('HitungFaktorial', function () {
     it('Test case 1: Faktorial dari 5', function () {
-        assert.strictEqual(faktorial(5), 120);
+        assert.strictEqual(faktorial(5), 120, "Salah pada testcase ke-1");
     });
 
     it('Test case 2: Faktorial dari 0', function () {
-        assert.strictEqual(faktorial(0), 1,);
+        assert.strictEqual(faktorial(0), 1, "Salah pada testcase ke-2");
     });
 
     it('Test case 3: Faktorial dari 1', function () {
-        assert.strictEqual(faktorial(1), 1,);
+        assert.strictEqual(faktorial(1), 1, "Salah pada testcase ke-3");
     });
 
+    it('Test case 4: Faktorial dari 3', function () {
+        assert.strictEqual(faktorial(3), 6, "Salah pada testcase ke-4");
+    });
+
+    it('Test case 5: Faktorial dari 6', function () {
+        assert.strictEqual(faktorial(6), 720, "Salah pada testcase ke-5");
+    });
 });
