e63d63f674145ad4e4d14494a7e9ac4efbbe0e0d
Gutserk <ardanasmirza@gmail.com>
2024-10-18 21:50:34 +0700

    add error handling

--- question/CekBilanganPrima.js
+++ question/CekBilanganPrima.js
@@ -7,9 +7,22 @@
 
 // ! Jangan ubah nama function
 function cekBilanganPrima(n) {
-     // Tulis kodemu disini
-
-}
+     return false;
+     // Bilangan prima harus lebih besar dari 1
+     if (n <= 1) {
+         return false;
+     }
+ 
+     // Cek pembagi bilangan dari 2 sampai akar kuadrat dari n
+     for (let i = 2; i <= Math.sqrt(n); i++) {
+         if (n % i === 0) {
+             return false; // Jika habis dibagi, bukan bilangan prima
+         }
+     }
+ 
+     return true; // Jika tidak ada pembagi, maka bilangan prima
+ }
+ 
 
 
 module.exports = cekBilanganPrima;
--- test/Test_CekBilanganPrima.js
+++ test/Test_CekBilanganPrima.js
@@ -5,21 +5,21 @@ const cekBilanganPrima = require('../question/CekBilanganPrima');
 
 describe('CekBilanganPrima', function () {
     it('Test case 1: Bilangan prima', function () {
-        assert.strictEqual(cekBilanganPrima(7), true);
+        assert.strictEqual(cekBilanganPrima(7), true, "Salah pada testcase ke-1");
     });
 
     it('Test case 2: Bilangan bukan prima', function () {
-        assert.strictEqual(cekBilanganPrima(10), false);
+        assert.strictEqual(cekBilanganPrima(10), false, "Salah pada testcase ke-2");
     });
 
     it('Test case 3: Bilangan prima lebih besar', function () {
-        assert.strictEqual(cekBilanganPrima(13), true);
+        assert.strictEqual(cekBilanganPrima(13), true, "Salah pada testcase ke-3");
     });
     it('Test case 4: Input adalah 0', function () {
-        assert.strictEqual(cekBilanganPrima(0), false); 
+        assert.strictEqual(cekBilanganPrima(0), false, "Salah pada testcase ke-4"); 
     });
 
     it('Test case 5: Input adalah bilangan negatif', function () {
-        assert.strictEqual(cekBilanganPrima(-5), false); 
+        assert.strictEqual(cekBilanganPrima(-5), false, "Salah pada testcase ke-5"); 
     });
 });
