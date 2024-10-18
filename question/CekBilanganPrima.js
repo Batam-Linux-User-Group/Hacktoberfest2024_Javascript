e7ad913878f756f64d30782a6fdec4a4d58872c0
Gutserk <ardanasmirza@gmail.com>
2024-10-18 21:52:03 +0700

    update question

--- question/CekBilanganPrima.js
+++ question/CekBilanganPrima.js
@@ -7,22 +7,9 @@
 
 // ! Jangan ubah nama function
 function cekBilanganPrima(n) {
-     return false;
-     // Bilangan prima harus lebih besar dari 1
-     if (n <= 1) {
-         return false;
-     }
- 
-     // Cek pembagi bilangan dari 2 sampai akar kuadrat dari n
-     for (let i = 2; i <= Math.sqrt(n); i++) {
-         if (n % i === 0) {
-             return false; // Jika habis dibagi, bukan bilangan prima
-         }
-     }
- 
-     return true; // Jika tidak ada pembagi, maka bilangan prima
- }
- 
+     // Tulis kodemu disini
+
+}
 
 
 module.exports = cekBilanganPrima;
