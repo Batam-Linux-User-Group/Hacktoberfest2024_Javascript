6ac2964f7351608e8222faf1e5673e720f004d98
Bayu Maulana <bayufransdo@gmail.com>
2024-10-19 10:42:54 +0700

    Saya mengerjakan soal CekBilanganPrima.js

--- question/CekBilanganPrima.js
+++ question/CekBilanganPrima.js
@@ -1,15 +1,15 @@
 /***
-*Buat fungsi yang memeriksa apakah sebuah angka merupakan bilangan prima.
-*Contoh:
-*Input: 7
-*Output: True
-*/
+ *Buat fungsi yang memeriksa apakah sebuah angka merupakan bilangan prima.
+ *Contoh:
+ *Input: 7
+ *Output: True
+ */
 
 // ! Jangan ubah nama function
 function cekBilanganPrima(n) {
-     // Tulis kodemu disini
+  // Tulis kodemu disini
 
+  console.log('Halo Nama saya bayu');
 }
-
 
 module.exports = cekBilanganPrima;
